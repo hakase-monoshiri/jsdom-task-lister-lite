ba34e8f3afe96612e8a11a727d53ff39180afb95
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-19 17:13:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.getElementById('create-task-form');
+
+  const tasks = document.getElementById('tasks');
+
+  form.addEventListener("click", function (event) { 
+
+    console.log("worked") ;
+
+    let input = document.getElementById('new-task-description').value; 
+
+    console.log(input);
+
+    const li = document.createElement("li");   
+    li.textContent = input;
+
+
+    tasks.appendChild(li)
+
+
+    event.preventDefault();
+  });
+
+
+
+
 });
